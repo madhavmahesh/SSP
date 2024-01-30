7a1a2791c8a129245efc7fd28b8acdc6ec005580
madhavmahesh <maheshmadhav28@gmail.com>
2024-01-30 15:12:43 -0800

    Update eligibility.component.ts
    pagination

--- src/app/eligibility/eligibility.component.ts
+++ src/app/eligibility/eligibility.component.ts
@@ -13,6 +13,16 @@ import { PortalService } from '../portal.service';
   ],
   template: `
     <section>
+    <div class="pagination">
+      <button (click)="previousPage()" [disabled]="currentPage === 1">
+        Previous
+      </button>
+      <span>Page {{ currentPage }} of {{ totalPages }}</span>
+      <button (click)="nextPage()" [disabled]="currentPage === totalPages">
+        Next
+      </button>
+    </div>
+
       <form>
         Enter SSN: <input type="text" placeholder="SSN" #filter>
         <button class="primary" type="button" (click)="filterResults(filter.value)">Fetch Data</button>
@@ -29,7 +39,7 @@ import { PortalService } from '../portal.service';
     </thead>
     </table>
     <app-search-result
-        *ngFor="let portalFields of filteredLocationList"
+        *ngFor="let portalFields of pagedLocationList"
         [portalFields]="portalFields">
       </app-search-result>
   `,
@@ -37,21 +47,61 @@ import { PortalService } from '../portal.service';
 })
 
 export class EligibilityComponent {
+
+   // Pagination variables
+   itemsPerPage = 3; // Set the number of items per page
+   currentPage = 1;
+   totalPages = 1;
+
   portalFieldsList: PortalFields[] = [];
   portalFields: PortalService = inject(PortalService);
   filteredLocationList: PortalFields[] = [];
+  pagedLocationList: PortalFields[] | undefined;
+
   constructor() {
     this.portalFieldsList = this.portalFields.getAllPortalFields();
     this.filteredLocationList = this.portalFieldsList;
+    this.updatePagedLocationList();
   }
   filterResults(text: string) {
     if (!text) {
       this.filteredLocationList = this.portalFieldsList;
       return;
     }
-
+  else{
     this.filteredLocationList = this.portalFieldsList.filter(
       portalFields => portalFields?.SSN.toLowerCase().includes(text.toLowerCase())
     );
+  }
+  
+    this.updatePagedLocationList();
+  }
+
+  private updatePagedLocationList() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    this.pagedLocationList = this.filteredLocationList.slice(
+      startIndex,
+      startIndex + this.itemsPerPage
+    );
+    this.totalPages = Math.ceil(
+      this.filteredLocationList.length / this.itemsPerPage
+    );
+    
+    
+  }
+
+  // Pagination methods
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePagedLocationList();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePagedLocationList();
+    }
   }
 }
